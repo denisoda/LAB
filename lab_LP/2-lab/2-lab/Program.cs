2a832edbf2ebc0b4ebded010eb04443e2fb0d407
ilya_dzeraziak <ilyadzeraziak@gmail.com>
2017-10-30 16:10:37 +0300

    Added 2-nd lab

---
+++ lab_LP/2-lab/2-lab/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace lab
+{
+	class MainClass
+	{
+
+
+		public static void Main (string[] args)
+		{
+
+			member m = new member ("");
+
+			StreamWriter write = new StreamWriter ("text.txt"); 
+				
+
+			write.WriteLine("sdfsdfsd");
+		}
+
+
+
+		class member{
+		
+			string name;
+
+			member()
+			{
+				name = "Ilya";
+			}
+
+		string inp_members(string name)
+		{
+				Console.WriteLine ("Inp name of member");
+				name = Console.ReadLine ();
+				return name;
+		}
+	}
+	}
+}
